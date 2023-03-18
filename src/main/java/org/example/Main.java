d737184a009da786bd24ba8c8201de3793d962b3
Timo Reiss <businesstimoreiss@gmail.com>
2023-03-18 21:31:06 +0100

    test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,10 +13,10 @@ public class Main {
         System.out.println("Retrieving match with id:" + matchHistoryIDs[0]);
         Match match = getSummonerInfo.getMatchHistoryData(MatchRegion.EUROPE, matchHistoryIDs[0]);
 
-//        for(String x: match.getMetadata().getParticipants()){
-//            Summoner summoner = getSummonerInfo.getSummonerNameByPuuid(x,Region.EUW1);
-//            System.out.println(summoner.getName());
-//        }
+        for(String x: match.getMetadata().getParticipants()){
+            Summoner summoner = getSummonerInfo.getSummonerNameByPuuid(x,Region.EUW1);
+            System.out.println(summoner.getName());
+        }
 
         match.getInfo().getParticipants().get(0).getKills();
         for(Participant participant: match.getInfo().getParticipants()){
